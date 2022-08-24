07ee59fad1c4989b1fb438be9915abda17ab9847
Fabian Bardos <fabian.bardos@gmail.com>
2022-08-24 22:29:01 +0200

    Modified DAG

--- dag_opendata_swiss.py
+++ dag_opendata_swiss.py
@@ -9,7 +9,7 @@ import requests
 from airflow import DAG
 from airflow.decorators import task
 from airflow.providers.mongo.hooks.mongo import MongoHook
-from airflow.providers.sftp.operators.sftp import SFTPOperator
+from airflow.providers.sftp.hooks.sftp import SFTPHook
 
 log = logging.getLogger(__name__)
 
@@ -23,6 +23,13 @@ with DAG(
     timestamp = dt.datetime.now(tz=dt.timezone.utc)
     filename = f'{timestamp.strftime("%Y-%m-%d")}-opendataswiss-extract.json'
     full_filepath = '/tmp/' + filename
+    remote_filepath = 'data/opendataswiss/'
+
+    url = 'https://opendata.swiss/api/3/action/package_list'
+    import_meta = {
+        'timestamp_imported': timestamp.isoformat(),
+        'extraction_url': url,
+    }
 
     @task(task_id='extract_packages')
     def extract_packages():
@@ -34,7 +41,6 @@ with DAG(
 
         # Count amount of packages in ckan
         PAGESIZE = 1000
-        url = 'https://opendata.swiss/api/3/action/package_list'
         count_packages = requests.get(url)
         pages = math.ceil(len(json.loads(count_packages.text)['result']) / PAGESIZE)
 
@@ -52,41 +58,34 @@ with DAG(
             pages_extract.append(page_response['result'])
         packages = list(itertools.chain(*pages_extract))
         log.info(f'Imported {len(packages)} packages from {url}.')
-        import_meta = {
-            'timestamp_imported': timestamp.isoformat(),
-            'extraction_url': url,
-        }
         return_dict = {
             'meta_data': import_meta,
             'data': packages,
         }
-        # FIX: This does not work. Use XCom instead. Tasks may run on completely different machines.
+
+        # Store to /tmp before transfer
         with open(full_filepath, 'w') as file:
             json.dump(return_dict, file)
 
-    t2 = SFTPOperator(
-        task_id='copy_to_nas69',
-        ssh_conn_id='sftp_nas69',
-        local_filepath=full_filepath,
-        remote_filepath='data/opendataswiss/' + filename,
-        create_intermediate_dirs=True,
-    )
+        # Store data directly after retrieval
+        sftp_client = SFTPHook('sftp_nas69')
+        sftp_client.store_file(remote_filepath + filename, full_filepath)
+
+        os.remove(full_filepath)  # Remove temporary stored file from /tmp
 
     @task(task_id='load_to_mongo')
     def load_to_mongo():
         with MongoHook('mongodb_u1082') as client:
+            sftp_client = SFTPHook('sftp_nas69')
+            sftp_client.retrieve_file(remote_filepath + filename, full_filepath)
             with open(full_filepath, 'r') as file:
                 doc = json.load(file)
                 log.info(f'EXTRACT from DOC: {str(doc)[:100]}')
-                for package in doc['data']['result']:
+                for package in doc['data']:
                     package['import_meta'] = import_meta
                     client.insert_one('opendata_swiss_packages', package)
-
-    @task(task_id='remove_local_file')
-    def remove_local_file():
-        os.remove(full_filepath)
+            os.remove(full_filepath)  # Remove temporary stored file from /tmp
 
     t1 = extract_packages()
-    t3 = load_to_mongo()
-    t4 = remove_local_file()
-    t1 >> [t2, t3] >> t4
+    t2 = load_to_mongo()
+    t1 >> t2
